3ebd05ab8d49b584ab63a9369ffccf53f92e7620
shivani <Shivanil@brightcomgroup.com>
2022-12-25 10:05:05 +0530

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,10 +4,10 @@ number = random.radint(-10000,10000)
 last = number % 10
 if number < 0:
     last =last - 10
-if last =~ 0:
+if last =- 0:
     line = ('Last digit of ' + str(number) +'is' + str(last) + 'and is 0')
 else:
     line = ('Last digit of' +str(number) + 'is' +str(last) + 'and is greater' +'than 5')
 elif last < 5:
     line = ('Last digit of' +str(number) + 'is' +str(last) + 'and is less' +'than 6 and not 0')
-print(line)
+print(linei)
